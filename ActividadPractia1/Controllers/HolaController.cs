e39ac967f610dfc5f332f5659b10fbccb3aedb0d
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-09 19:12:42 -0400

    HolaController

---
+++ ActividadPractia1/Controllers/HolaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HolaController : ControllerBase
+    {
+        [HttpGet("saludo")]
+        public IActionResult Saludo([FromQuery] string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                return BadRequest(new { mensaje = "Nombre es obligatorio" });
+            }
+
+            return Ok(new
+            {
+                mensaje = $"Bienvenido/a {nombre} a API REST"
+            });
+        }
+    }
+}
