6282dad02e6a1a0c8723d6da521aeda13db2ab20
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-10 21:45:18 -0400

    PropinaController

---
+++ ActividadPractia1/Controllers/PropinaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PropinaController : ControllerBase
+    {
+        // GET: api/propina/calcular?monto=1000&porcentaje=10
+        [HttpGet("calcular")]
+        public IActionResult Calcular([FromQuery] double monto, [FromQuery] double porcentaje)
+        {
+            if (monto < 0 || porcentaje < 0)
+            {
+                return BadRequest(new { mensaje = "no puede ingresar numeros menor a cerp" });
+            }
+
+            double propina = monto * (porcentaje / 100);
+            double cuenta = monto + propina;
+
+            return Ok(new
+            {
+                subtotal = monto,
+                calduloPropina = propina,
+                totalPago = cuenta
+            });
+        }
+    }
+}
