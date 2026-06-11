055f8b9cc2e7e4a79afa982b1b669a241949df27
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-10 22:11:44 -0400

    NotasController

---
+++ ActividadPractia1/Controllers/NotasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    public class RegistroNotas
+    {
+        public List<int> ListaNotas { get; set; } = new List<int>();
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotasController : ControllerBase
+    {
+        // POST: api/notas/estadisticas
+        [HttpPost("estadisticas")]
+        public IActionResult ObtenerEstadisticas([FromBody] RegistroNotas datos)
+        {
+            if (datos == null || datos.ListaNotas == null || datos.ListaNotas.Count == 0)
+            {
+                return BadRequest(new { mensaje = "envie una calificación/nota" });
+            }
+
+            double promedio = datos.ListaNotas.Average();
+            int mayor = datos.ListaNotas.Max();
+            int menor = datos.ListaNotas.Min();
+
+            int aprobados = datos.ListaNotas.Count(n => n >= 70);
+            int reprobados = datos.ListaNotas.Count(n => n < 70);
+
+            return Ok(new
+            {
+                promedioGeneral = Math.Round(promedio, 2),
+                notaAlta = mayor,
+                notaBaja = menor,
+                totalAprobados = aprobados,
+                totalReprobados = reprobados
+            });
+        }
+    }
+}
