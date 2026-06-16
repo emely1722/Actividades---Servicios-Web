4ab3a76143ec250b3eb626efc729d34395de4ff9
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-15 22:02:50 -0400

    Creación lista estática para almacenamiento temporal

--- ActividadPractica2/Controllers/EstudiantesController.cs
+++ ActividadPractica2/Controllers/EstudiantesController.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ActividadPractica2.Models;
 
 namespace ActividadPractica2.Controllers
 {
-    public class EstudiantesController : Controller
+    [ApiController]
+    [Route("api/estudiantes")]
+    public class EstudiantesController : ControllerBase
     {
-        public IActionResult Index()
+        private static readonly List<Estudiante> _estudiantesMemoria = new List<Estudiante>
         {
-            return View();
-        }
+            new Estudiante { Id = 1, Nombre = "Ana", Apellido = "Pérez", Correo = "ana.perez@ufhec.edu.do", Carrera = "Ingeniería de Sistemas", Edad = 20, Promedio = 85.5m, Activo = true },
+            new Estudiante { Id = 2, Nombre = "Pedro", Apellido = "Martínez", Correo = "pedro.m@ufhec.edu.do", Carrera = "Contabilidad", Edad = 23, Promedio = 72.0m, Activo = true },
+            new Estudiante { Id = 3, Nombre = "Lucas", Apellido = "Díaz", Correo = "lucas.d@ufhec.edu.do", Carrera = "Ingeniería de Sistemas", Edad = 21, Promedio = 64.5m, Activo = false }
+        };
+
     }
 }
