7f1b25a740fc57727e5e8d2f904a82a7bbfd4446
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-21 20:53:05 -0400

    modificación program.cs y añadiendo el AppDbContext

---
+++ ActividadPractica3/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ActividadPractica3.Models;
+
+namespace ActividadPractica3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
