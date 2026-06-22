b8483703e6596f4b819cf58d74f5d230f48a4e77
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-21 20:00:13 -0400

    creación del modelo Autor.cs

---
+++ ActividadPractica3/Models/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActividadPractica3.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre requerido")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nombre debe ocupar de 3-100 caracteres")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Nacionalidad requerida")]
+        [StringLength(50, ErrorMessage = "No puede exceder 50 caracteres")]
+        public string Nacionalidad { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Año nacimiento requerido")]
+        [Range(1500, 2100, ErrorMessage = "Debe estar entre 1500-2100")]
+        public int Nacimiento { get; set; }
+    }
+}
