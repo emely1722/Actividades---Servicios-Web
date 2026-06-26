162899819772fba71aa9678173bd55889327d262
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-06-25 21:15:58 -0400

    actualizacion modelo

--- ActividadPractica3.1/Models/Libros.cs
+++ ActividadPractica3.1/Models/Libros.cs
@@ -1,30 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ActividadPractica3._1.Models
+﻿namespace ActividadPractica3._1.Models
 {
     public class Libro
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(250)]
         public string Titulo { get; set; } = string.Empty;
-
-        [Required]
-        [StringLength(150)]
         public string Autor { get; set; } = string.Empty;
-
         public int AnioPublicacion { get; set; }
-
-        [Required]
-        [StringLength(100)]
         public string Genero { get; set; } = string.Empty;
-
         public int NumeroPaginas { get; set; }
-
         public decimal Precio { get; set; }
-
         public bool Disponible { get; set; }
     }
 }
